db5f258446311ebb0fa039b8c9d90413c1804f53
YisusBk <masterbk12@gmail.com>
2020-06-16 19:45:43 -0400

    Dao Task Complete

--- app/src/main/java/com/nico/taskapp/model/TaskDao.kt
+++ app/src/main/java/com/nico/taskapp/model/TaskDao.kt
@@ -1,4 +1,21 @@
 package com.nico.taskapp.model
 
+import androidx.room.*
+
+@Dao
 interface TaskDao {
+    @Query("SELECT * FROM task_entity")
+    fun getAllTask(): MutableList<Task>
+
+    @Insert
+    fun addTask(taskEntity : Task):Long
+
+    @Query("SELECT * FROM task_entity where id like :id")
+    fun getTaskById(id: Long): Task
+
+    @Update
+    fun updateTask(taskEntity: Task):Int
+
+    @Delete
+    fun deleteTask(taskEntity: Task):Int
 }
