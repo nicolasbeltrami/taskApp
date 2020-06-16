12e384aadcc459944b66061e36c4a8eee3a6686f
YisusBk <masterbk12@gmail.com>
2020-06-16 19:46:43 -0400

    Database task complete

--- app/src/main/java/com/nico/taskapp/model/TaskDataBase.kt
+++ app/src/main/java/com/nico/taskapp/model/TaskDataBase.kt
@@ -1,4 +1,9 @@
 package com.nico.taskapp.model
 
-class TaskDataBase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities=[Task :: class], version = 1)
+abstract class TaskDataBase : RoomDatabase() {
+    abstract fun TaskDao():TaskDao
 }
