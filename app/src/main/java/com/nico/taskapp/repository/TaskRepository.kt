77e3aef5a8d26164bac4bf72c50ee5c529232913
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-06-17 20:09:06 -0400

    repository creado

---
+++ app/src/main/java/com/nico/taskapp/repository/TaskRepository.kt
@@ -0,0 +1,4 @@
+package com.nico.taskapp.repository
+
+class TaskRepository {
+}
--- app/src/main/java/com/nico/taskapp/ui/list/TaskAdapter.kt
+++ app/src/main/java/com/nico/taskapp/ui/list/TaskAdapter.kt
@@ -1,2 +1,25 @@
 package com.nico.taskapp.ui.list
 
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nico.taskapp.model.Task
+
+class TaskViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+}
+
+class TaskAdapter() : ListAdapter<Task, TaskViewHolder>(DIFF_CONFIG) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
+
+}
