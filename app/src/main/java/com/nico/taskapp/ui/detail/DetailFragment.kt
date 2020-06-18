e4d9b9def62f7fa24f8252d49b56e2c067e58f74
YisusBk <masterbk12@gmail.com>
2020-06-17 20:06:43 -0400

    Instance created

--- app/src/main/java/com/nico/taskapp/ui/detail/DetailFragment.kt
+++ app/src/main/java/com/nico/taskapp/ui/detail/DetailFragment.kt
@@ -6,20 +6,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.nico.taskapp.R
+import com.nico.taskapp.model.Task
 import kotlinx.android.synthetic.main.fragment_detail.*
+import kotlinx.coroutines.coroutineScope
 
 class DetailFragment : Fragment() {
-
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_detail, container, false)
     }
 
-    val name = etName.text.toString()
-    val content = etContent.text.toString()
 
 }
