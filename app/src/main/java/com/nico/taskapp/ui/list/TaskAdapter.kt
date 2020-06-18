8802de7acd8f4eada0512d378f73dbe696339cb5
Nico Beltrami <nicolas.beltrami@gmail.com>
2020-06-17 20:27:59 -0400

    Desarrollo task adapter

--- app/src/main/java/com/nico/taskapp/ui/list/TaskAdapter.kt
+++ app/src/main/java/com/nico/taskapp/ui/list/TaskAdapter.kt
@@ -1,2 +1,22 @@
 package com.nico.taskapp.ui.list
 
+import android.view.View
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.nico.taskapp.model.Task
+
+class TaskAdapter : ListAdapter<Task, TaskViewHolder>() {
+
+}
+
+class TaskDiffCallback : DiffUtil.ItemCallback<Task>(){
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem === newItem
+    }
+
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
+
+}
